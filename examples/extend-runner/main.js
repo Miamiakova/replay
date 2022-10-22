caef408db0c1f565efc17713f79eca74ad11d0d0
Miamiakova <116422753+Miamiakova@users.noreply.github.com>
2022-10-22 08:28:39 +0100

    Update main.js

--- examples/extend-runner/main.js
+++ examples/extend-runner/main.js
@@ -27,6 +27,35 @@ class Extension extends PuppeteerRunnerExtension {
     await super.afterAllSteps(flow);
     console.log('done');
   }
+}import { createRunner, PuppeteerRunnerExtension } from '@puppeteer/replay';
+import puppeteer from 'puppeteer';
+
+const browser = await puppeteer.launch({
+  headless: true,
+});
+
+const page = await browser.newPage();
+
+class Extension extends PuppeteerRunnerExtension {
+  async beforeAllSteps(flow) {
+    await super.beforeAllSteps(flow);
+    console.log('starting');
+  }
+
+  async beforeEachStep(step, flow) {
+    await super.beforeEachStep(step, flow);
+    console.log('before', step);
+  }
+
+  async afterEachStep(step, flow) {
+    await super.afterEachStep(step, flow);
+    console.log('after', step);
+  }
+
+  async afterAllSteps(flow) {
+    await super.afterAllSteps(flow);
+    console.log('done');
+  }
 }
 
 const runner = await createRunner(
@@ -40,6 +69,23 @@ const runner = await createRunner(
     ],
   },
   new Extension(browser, page, 7000)
+);
+
+await runner.run();
+
+await browser.close();
+
+const runner = await createRunner(
+  {
+    title: 'Test recording',
+    steps: [
+      {
+        type: 'navigate',
+        url: 'https://wikipedia.org',
+      },
+    ],
+  },
+  new Extension(browser, page, 9000)
 );
 
 await runner.run();
